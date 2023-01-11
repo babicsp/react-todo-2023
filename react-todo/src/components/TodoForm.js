01f5d153e6a47858fa1921904f8a2d9745226628
babicsp <babics.peter@gmail.com>
2023-01-11 22:23:59 +0100

    first app

---
+++ react-todo/src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React, {usedState} from 'react'
+
+function TodoForm() {
+const [input, setInputs] = usedState('')
+  return (
+    <form className='todo-form'>
+        <input type='text' placeholder='Add a todo' value={input}
+        name='text' className='todo-input'/>
+    </form>
+  )
+}
+
+export default TodoForm
