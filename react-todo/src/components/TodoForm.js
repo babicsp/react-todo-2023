43d5e1f8ec85dbf40f3e1a596589568d98ed8297
babicsp <babics.peter@gmail.com>
2023-01-11 22:30:43 +0100

    formatting on todoForm

--- react-todo/src/components/TodoForm.js
+++ react-todo/src/components/TodoForm.js
@@ -1,13 +1,18 @@
-import React, {usedState} from 'react'
+import React, { usedState } from "react";
 
 function TodoForm() {
-const [input, setInputs] = usedState('')
+  const [input, setInputs] = usedState("");
   return (
     <form className='todo-form'>
-        <input type='text' placeholder='Add a todo' value={input}
-        name='text' className='todo-input'/>
+      <input
+        type='text'
+        placeholder='Add a todo'
+        value={input}
+        name='text'
+        className='todo-input'
+      />
     </form>
-  )
+  );
 }
 
-export default TodoForm
+export default TodoForm;
