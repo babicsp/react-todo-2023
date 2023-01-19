c1320ff3eb2491cbf731c96052ab4a11ef109f64
babicsp <babics.peter@gmail.com>
2023-01-19 13:17:25 +0100

    20:00

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoForm from './components/TodoForm';
+// import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className='todo-app'>
-        <TodoForm></TodoForm>
+        <TodoList />
       </div>
     );
   }
